30b4af80fc1e5f7ece92044b6aa5f2f8f4fe5707
SangYeonY <sa9798@naver.com>
2022-03-15 00:02:15 +0900

    day6_js오브젝트

---
+++ js_day06/4_string메소드.js
@@ -0,0 +1,22 @@
+let text1 = "John Doe";
+console.log(text1);
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"'; 
+
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
+
+let text2 ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(text2.length);
+
+let text3 = "We are the so-called \"Vikings\" from the north.";
+console.log(text3);
+
+let x = "john";
+let y = new String("john");
+console.log(typeof x);
+console.log(typeof y);
+console.log(x==y);
