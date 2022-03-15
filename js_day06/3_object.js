8031a1725eccc14d0eb419f0f26ed4159b791de0
SangYeonY <sa9798@naver.com>
2022-03-15 17:17:25 +0900

    day07_유효성검사끝

--- js_day06/3_object.js
+++ js_day06/3_object.js
@@ -13,10 +13,11 @@ let person = { name : 'sana',
                 age : 19,
                 sayHi : function(){                 // 메소드 정의 : 객체의 프로퍼티값이 함수 정의일 때 sayHI는 메소드 입니다.
                     console.log(`Hi!! 나는 ${this.name} 입니다.`);      
+                    console.log('Hi!! 나는 + this.name + 입니다.');      
                 }
             };        // 객체리터럴을 이용하여 객체 생성
 
-        person.sayHi();     // 객체의 메소드 실행
+        person.sayHi();     // 객체의 메소드 실행, this는 person 변수가 참조하는 객체.
 
 //프로퍼티 접근하여 person 객체의 이름변경
 person.name = '사나';   // 방법1
--- js_day06/4_string메소드.js
+++ js_day06/4_string메소드.js
@@ -20,3 +20,35 @@ let y = new String("john");
 console.log(typeof x);
 console.log(typeof y);
 console.log(x==y);
+
+let str = "Apple, Banana, Kiwi";    // " 는 0부터 계산
+console.log(str.slice(7,13));
+console.log(str.slice(-12,-6));     // 음수일때 문자열 끝부터 12번째 위치 6번째 위치 잘라냄
+console.log(str.slice(7));          // 두번째 매개변수 생략시 나머지 문자열 잘라냄
+console.log(str.slice(-12));        // 위와 같은값
+
+
+console.log(str.substring(7,13));   // slice 와 차이점은 음수를 매개변수로 사용 못함
+console.log(str.substr(7,6));        // slice 와 차이점은 두번째 매개변수가 추출된 부분의 길이를 지정한다.
+console.log(str.substr(7));
+
+/*
+-string 주요 메소드
+slice(start,end)  : start,end 인덱스가 음수가 될수 있음.이때는 맨끝 부터 -1
+substring(start,end)  : end 인덱스 문자는 포함 안함
+substr(start,length)  : 두번재 인자가 추출할 길이
+replace(src,dest)   : src문자열을 dest로 대체함.
+toUpperCase()
+toLowerCase()
+concat(join,str2) :  str2문자열과 연결함. join문자열은 연결할때 사용할 문자열
+padStart()  : 예제 참고
+padEnd() : 예제 참고
+charAt(index) : index위치의 문자 구하기, 
+     - 자바스크립트에서는 문자열을 배열처럼 다룰수 있음. [] 안에 인덱스지정 가능.
+split() : 문자열 나누기(구분기호 사용)
+문자열검색 메소드 : 사용형식은 자바와 동일.
+String indexOf()
+String lastIndexOf()
+String startsWith()
+String endsWith()
+*/
